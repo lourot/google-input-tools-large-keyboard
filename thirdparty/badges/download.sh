205e36e0b3783816bb89a6ec4c0be0de33e70178
Aurelien Lourot <aurelien.lourot@gmail.com>
2018-07-23 15:35:41 +0300

    Cache flaky badges.

---
+++ thirdparty/badges/download.sh
@@ -0,0 +1,3 @@
+# We cache some flaky badges to work around https://github.com/badges/shields/issues/1732 :
+curl -o users.svg https://img.shields.io/chrome-web-store/users/gjhnbhjegnfhehpjhkajgfbkcbpmomoh.svg
+curl -o rating.svg https://img.shields.io/chrome-web-store/rating/gjhnbhjegnfhehpjhkajgfbkcbpmomoh.svg
--- thirdparty/download.sh
+++ thirdparty/download.sh
@@ -1,3 +1,9 @@
+set -e
+
 for size in 128 280; do
   curl -o "icon$size.png" "https://png.icons8.com/?id=9747&size=$size"
 done
+
+pushd badges/
+./download.sh
+popd
