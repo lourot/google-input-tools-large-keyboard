86a519a49cda65d965f90a34116c96f446ff7157
Aurelien Lourot <software@lourot.dev>
2024-02-05 23:29:42 +0100

    qual: improve popup

--- popup.js
+++ popup.js
@@ -1,18 +1,17 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   var manifest = chrome.runtime.getManifest();
   document.body.innerHTML = `
-<h1>${manifest.name}</h1> // ${manifest.version}
-<h2>Find us:</h2>
-<a target="_blank" href="https://chrome.google.com/webstore/detail/large-keyboard-for-google/gjhnbhjegnfhehpjhkajgfbkcbpmomoh">On the Web Store</a><br />
-<a target="_blank" href="https://github.com/AurelienLourot/google-input-tools-large-keyboard">On GitHub</a><br />
-<a target="_blank" href="mailto:aurelien.lourot@gmail.com">Via e-mail</a> // aurelien.lourot@gmail.com
+<h2>${manifest.name} v${manifest.version}</h2>
+<div><a target="_blank" href="https://chrome.google.com/webstore/detail/large-keyboard-for-google/gjhnbhjegnfhehpjhkajgfbkcbpmomoh">Web Store / Download</a></div>
+<div><a target="_blank" href="https://github.com/AurelienLourot/google-input-tools-large-keyboard">GitHub / Source code</a></div>
+<div><a target="_blank" href="https://lourot.dev/support">Contact / Support / Donate</a></div>
 
 <h2>FAQ:</h2>
-<p class="question">Q: Where is the keyboard?</p>
-<p class="answer">
+<div class="question">Q: Where is the keyboard?</div>
+<div>
   A: Install
   <a target="_blank" href="https://chrome.google.com/webstore/detail/google-input-tools/mclkkofklkfljcocdinagocijmpgbhab">
     Google Input Tools</a>.
-</p>
+</div>
 `;
 });
